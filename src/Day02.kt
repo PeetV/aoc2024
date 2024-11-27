4ffa10ca7734a809e061342f1752d8da9fa1fe43
PeetV <peetjvermeulen@gmail.com>
2024-11-28 08:42:58 +1100

    Template updates

--- src/Day02.kt
+++ src/Day02.kt
@@ -9,11 +9,11 @@ fun main() {
     }
 
     // Test input from the `src/Day01_test.txt` file
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 3)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 1)
 
     // Input from the `src/Day01.txt` file
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }
