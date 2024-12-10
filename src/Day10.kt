ee329bacbb891eb605765d20484a11abbd1ab744
PeetV <peetjvermeulen@gmail.com>
2024-12-10 21:10:21 +1100

    Day 10 wip

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,4 +1,5 @@
 import ktml.NodeMappedGraph
+import ktml.PathTree
 import kotlin.math.abs
 
 fun main() {
@@ -12,7 +13,8 @@ fun main() {
         // Add edges
         for ((columnIndex, rowIndex) in grid.indices) {
             val location = XYLocation(columnIndex, rowIndex)
-            val locationVal = grid.getCharacter(location.xy).toString().toInt()
+            val char = grid.getCharacter(location.xy)
+            val locationVal = char.toString().toInt()
             for (direction in listOf(Direction.North, Direction.South, Direction.East, Direction.West)) {
                 val nextLocation = location.nextLocation(direction)
                 if (grid.isInBounds(nextLocation.xy)) {
@@ -46,16 +48,37 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val grid = CharacterGrid(input)
+        val graph = buildGraph(grid)
+        val zeroLocations = grid.findLocations('0').map { XYLocation(it.first, it.second) }
+        val nineLocations = grid.findLocations('9').map { XYLocation(it.first, it.second) }
+        var result = 0
+        for (zeroLocation in zeroLocations) {
+            for (nineLocation in nineLocations) {
+                if (abs(zeroLocation.x - nineLocation.x) > 10) continue
+                if (abs(zeroLocation.y - nineLocation.y) > 10) continue
+                val pathTree = PathTree(zeroLocation)
+                val buildResult = pathTree.buildPathTree(
+                    graph, fromNode = zeroLocation, toNode = nineLocation, maxSteps = 10_000, getDistance = { it })
+                if (buildResult.isSuccess) {
+                    val paths = pathTree.enumeratePaths()
+                    if (paths.isSuccess)
+                        result += paths.getOrThrow().count()
+                }
+            }
+        }
+        return result
     }
 
     // Test input from the `src/Day10_test.txt` file
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 36)
+//    check(part1(testInput) == 36)
+    println(part2(testInput))
+//    check(part2(testInput) == 81)
 
     // Input from the `src/Day10.txt` file
-    val input = readInput("Day10")
-    part1(input).println()
+//    val input = readInput("Day10")
+//    part1(input).println()
 //    part2(input).println()
 
 }
