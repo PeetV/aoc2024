358cc991bdbe825c6520ad3ccfb6694390189443
PeetV <peetjvermeulen@gmail.com>
2024-12-15 16:34:51 +1100

    Day 15 part 1 wip

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,6 +1,14 @@
 fun main() {
 
+    fun processFile(input: List<String>): Pair<CharacterGrid, String> {
+        val blankLine = input.indexOfFirst { it.isBlank() }
+        val grid = CharacterGrid(input.slice(0..<blankLine))
+        val moves = input.slice((blankLine + 1)..input.lastIndex).joinToString("")
+        return grid to moves
+    }
+
     fun part1(input: List<String>): Int {
+        val (grid, moves) = processFile(input)
         return input.size
     }
 
@@ -8,13 +16,18 @@ fun main() {
         return input.size
     }
 
-    // Test input from the `src/Day15_test.txt` file
-    val testInput = readInput("Day15_test")
-    check(part1(testInput) == 1)
+    // Test input from the `src/Day15_test1.txt` file
+    var testInput = readInput("Day15_test1")
+    part1(testInput).println()
+//    check(part1(testInput) == 1)
+
+    // Test input from the `src/Day15_test2.txt` file
+    testInput = readInput("Day15_test2")
+//    check(part1(testInput) == 1)
 
     // Input from the `src/Day15.txt` file
-    val input = readInput("Day15")
-    part1(input).println()
-    part2(input).println()
+//    val input = readInput("Day15")
+//    part1(input).println()
+//    part2(input).println()
 
 }
